302691521701f7486ad751c15ab9c81493eed20d
Ivan Marcellino <ivangoivan@gmail.com>
2025-09-28 11:12:39 +0700

    no semicolon syntax

--- hello_world/main.rs
+++ hello_world/main.rs
@@ -1,3 +1,22 @@
+// fn main() {
+//     println!("Hello world!")
+// }
+
+// function with no `;` means return
+fn five() -> i32 {
+    5
+}
+
 fn main() {
-    println!("Hello world!")
+    let x = five();
+
+    println!("The value of x is: {x}");
+
+    // assign a value after some computation
+    let something = {
+        let x = 100;
+        x+2
+    };
+
+    println!("{something}");
 }
